92b6402914239ff0e10ef3c56aaed765f366b3b6
Traveler <63530343+Traveler-NH@users.noreply.github.com>
2020-08-23 19:30:42 +0800

    Add files via upload

---
+++ wxpython_socket_client.py
@@ -0,0 +1,23 @@
+import wx,socket,threading
+client=socket.socket()
+client.connect(('192.168.0.103',25561))
+def send(none): #定义发送信息的函数。
+    client.send(('客户1：'+textctrl_send.Value).encode('utf-8'))
+    textctrl_send.Value=''
+def recv(): #定义函数给子线程接收信息。
+    while True:
+        textctrl_message.write(client.recv(1024*1024*1024).decode('utf-8')+'\n') #把接收到的信息加入到文本框里。
+app=wx.App(False)
+frame=wx.Frame(parent=None,id=wx.ID_ANY,title='client',size=(800,500),)
+frame.Centre() #使框架位居屏幕中间。
+frame.SetMaxSize((800,500)) #固定框架的大小，使之无法拉伸和最大化，但可以收缩和最小化。
+button_no=wx.Button(parent=frame,id=0)
+button_no.Show(False) #隐藏按钮“button_no”。
+button_send=wx.Button(parent=frame,id=1,label='发送',pos=(660,410),size=(90,40),style=0)
+textctrl_send=wx.TextCtrl(parent=frame,id=2,pos=(5,240),size=(780,160),style=wx.TE_MULTILINE)
+textctrl_message=wx.TextCtrl(parent=frame,id=2,pos=(5,0),size=(780,220),style=wx.TE_MULTILINE)
+button_send.Bind(event=wx.EVT_BUTTON,handler=send)
+frame.Show(True) #显示框架
+recv=threading.Thread(target=recv)
+recv.start()
+app.MainLoop()
