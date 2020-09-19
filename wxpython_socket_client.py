512bd83e7c98598b95bc4c649b757bb3ef394b6a
Traveler <63530343+Traveler-NH@users.noreply.github.com>
2020-09-19 18:20:22 +0800

    Update wxpython_socket_client.py

--- wxpython_socket_client.py
+++ wxpython_socket_client.py
@@ -8,7 +8,7 @@ def recv(): #定义函数给子线程接收信息。
     while True:
         textctrl_message.write(client.recv(1024*1024*1024).decode('utf-8')+'\n') #把接收到的信息加入到文本框里。
 app=wx.App(False)
-frame=wx.Frame(parent=None,id=wx.ID_ANY,title='client',size=(800,500),)
+frame=wx.Frame(parent=None,id=wx.ID_ANY,title='client',size=(800,500))
 frame.Centre() #使框架位居屏幕中间。
 frame.SetMaxSize((800,500)) #固定框架的大小，使之无法拉伸和最大化，但可以收缩和最小化。
 button_no=wx.Button(parent=frame,id=0)
