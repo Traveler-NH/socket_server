aea68b4f6b0cc568d38ad810f66c123f1516e086
Traveler <63530343+Traveler-NH@users.noreply.github.com>
2020-10-02 20:49:05 +0800

    Update wxpython_socket_client.py

--- wxpython_socket_client.py
+++ wxpython_socket_client.py
@@ -11,8 +11,6 @@ app=wx.App(False)
 frame=wx.Frame(parent=None,id=wx.ID_ANY,title='client',size=(800,500))
 frame.Centre() #使框架位居屏幕中间。
 frame.SetMaxSize((800,500)) #固定框架的大小，使之无法拉伸和最大化，但可以收缩和最小化。
-button_no=wx.Button(parent=frame,id=0)
-button_no.Show(False) #隐藏按钮“button_no”。
 button_send=wx.Button(parent=frame,id=1,label='发送',pos=(660,410),size=(90,40),style=0)
 textctrl_send=wx.TextCtrl(parent=frame,id=2,pos=(5,240),size=(780,160),style=wx.TE_MULTILINE)
 textctrl_message=wx.TextCtrl(parent=frame,id=2,pos=(5,0),size=(780,220),style=wx.TE_MULTILINE)
